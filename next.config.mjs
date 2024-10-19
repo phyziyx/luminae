fbf6ac8baf494f2617a44daab0f38208a3291657
phyziyx <96657278+phyziyx@users.noreply.github.com>
2024-10-20 02:08:46 +0500

    add zod, shadcn, prisma, react hook forms

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: [
+			'files.stripes.com',
+			'img.clerk.com'
+		]
+	},
+	reactStrictMode: 'true'
+};
 
 export default nextConfig;
