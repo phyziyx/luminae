bfd602afd6814df7ce3dde854dd9acbb4875ac11
phyziyx <96657278+phyziyx@users.noreply.github.com>
2024-10-28 20:22:02 +0500

    wip: invitation form design

--- src/app/(auth)/accept-invitation/page.tsx
+++ src/app/(auth)/accept-invitation/page.tsx
@@ -3,14 +3,58 @@
 import * as React from "react";
 import { useSignUp, useUser } from "@clerk/nextjs";
 import { useSearchParams, useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
+import Logo from "@/components/logo";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/ui/icons";
+
+const formSchema = z.object({
+  firstName: z.string().min(2, {
+    message: "First name must be at least 2 characters.",
+  }),
+  lastName: z.string().min(2, {
+    message: "Last name must be at least 2 characters.",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+});
 
 export default function Page() {
+  const t = useTranslations();
+
   const { user } = useUser();
   const router = useRouter();
   const { isLoaded, signUp, setActive } = useSignUp();
-  const [firstName, setFirstName] = React.useState("");
-  const [lastName, setLastName] = React.useState("");
-  const [password, setPassword] = React.useState("");
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      password: "",
+    },
+  });
 
   // Handle signed-in users visiting this page
   // This will also redirect the user once they finish the sign-up process
@@ -24,14 +68,12 @@ export default function Page() {
   const token = useSearchParams().get("__clerk_ticket");
 
   // If there is no invitation token, restrict access to this page
-  if (!token) {
+  if (token) {
     return <p>No invitation token found.</p>;
   }
 
   // Handle submission of the sign-up form
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     if (!isLoaded) return;
 
     try {
@@ -44,9 +86,9 @@ export default function Page() {
       const signUpAttempt = await signUp.create({
         strategy: "ticket",
         ticket: token,
-        firstName,
-        lastName,
-        password,
+        firstName: values.firstName,
+        lastName: values.lastName,
+        password: values.password,
       });
 
       // If the sign-up was completed, set the session to active
@@ -63,43 +105,80 @@ export default function Page() {
   };
 
   return (
-    <>
-      <h1>Sign up</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="firstName">Enter first name</label>
-          <input
-            id="firstName"
-            type="text"
-            name="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="lastName">Enter last name</label>
-          <input
-            id="lastName"
-            type="text"
-            name="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Enter password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <button type="submit">Next</button>
-        </div>
-      </form>
-    </>
+    <div className="grid w-full grow items-center justify-center px-4 sm:justify-center">
+      <Logo className="text-blue-500" />
+
+      <Card className="w-full sm:w-96">
+        <CardHeader>
+          <CardTitle>{t("CREATE_ACCOUNT_HEADER")}</CardTitle>
+          <CardDescription>{t("CREATE_ACCOUNT_DESCRIPTION")}</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-y-4">
+          <div className="grid w-full gap-y-4">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-2"
+              >
+                <FormField
+                  control={form.control}
+                  name="firstName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="firstName">
+                        Enter first name
+                      </FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="lastName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="lastName">Enter last name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="password">Enter password</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="grid w-full gap-y-4">
+            <Button type="submit">
+              {false ? (
+                <Icons.spinner className="size-4 animate-spin" />
+              ) : (
+                t("CONTINUE")
+              )}
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
