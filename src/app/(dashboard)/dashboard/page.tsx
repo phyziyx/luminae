a6275dea71e422b7ec3ac1307f65eb7965234e37
phyziyx <96657278+phyziyx@users.noreply.github.com>
2024-11-05 00:19:24 +0500

    almost completed Dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -12,20 +12,15 @@ import {
 } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import { Separator } from "@/components/ui/separator";
-import { DollarSign, Goal, Contact2 } from "lucide-react";
+import { DollarSign, Goal, UsersRoundIcon } from "lucide-react";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
+import { getTranslations } from "next-intl/server";
 
 const Dashboard = async () => {
   const { userId } = await auth();
   const user = await currentUser();
+
+  const t = await getTranslations();
 
   if (!userId || !user) {
     return <div>Not authenticated!</div>;
@@ -33,156 +28,111 @@ const Dashboard = async () => {
 
   const currentYear = new Date().getFullYear();
 
+  const income = Intl.NumberFormat("en-US").format(1337);
+  const potentialIncome = Intl.NumberFormat("en-US").format(4200);
+  const activeClients = 12;
+  const goalProgress = 6;
+  const currentGoal = 9;
+
+  const progressPercent = (goalProgress / currentGoal) * 100;
+
   return (
     <>
       <header className="flex h-16 shrink-0 items-center gap-2">
         <div className="flex items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
+          <h1 className="text-3xl font-semibold">{t("DASHBOARD")}</h1>
         </div>
       </header>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          {[].map((page) => (
-            <div key={page.id!} className="aspect-video rounded-xl bg-muted/50">
-              {page.name!}
-            </div>
-          ))}
-        </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      </div>
-    </>
-  );
+        <div className="grid auto-rows-min gap-4 md:grid-cols-4">
+          <Card className="bg-muted/50 dark:bg-muted flex-1 relative">
+            <CardHeader>
+              <CardDescription>{t("INCOME")}</CardDescription>
+              <CardTitle className="text-4xl">${income}</CardTitle>
+              <small className="text-xs text-muted-foreground">
+                {t("FOR_THE_YEAR", {
+                  YEAR: currentYear,
+                })}
+              </small>
+            </CardHeader>
+            <CardContent className="text-sm text-muted-foreground">
+              {t("INCOME_DESCRIPTION")}
+            </CardContent>
+            <DollarSign className="absolute right-4 top-4 text-muted-foreground" />
+          </Card>
 
-  return (
-    <div className="relative h-full">
-      <h1 className="text-4xl">Dashboard</h1>
-      <Separator className="my-6" />
-      <div className="flex flex-col gap-4 pb-6">
-        <div className="flex gap-4 flex-col xl:!flex-row">
-          <Card className="flex-1 relative">
+          <Card className="bg-muted/50 dark:bg-muted flex-1 relative">
             <CardHeader>
-              <CardDescription>Income</CardDescription>
-              <CardTitle className="text-4xl">$0.00</CardTitle>
+              <CardDescription>{t("POTENTIAL_INCOME")}</CardDescription>
+              <CardTitle className="text-4xl">${potentialIncome}</CardTitle>
               <small className="text-xs text-muted-foreground">
-                For the year {currentYear}
+                {t("FOR_THE_YEAR", {
+                  YEAR: currentYear,
+                })}
               </small>
             </CardHeader>
             <CardContent className="text-sm text-muted-foreground">
-              Total revenue generated as reflected in your stripe dashboard.
+              {t("POTENTIAL_INCOME_DESCRIPTION")}
             </CardContent>
             <DollarSign className="absolute right-4 top-4 text-muted-foreground" />
           </Card>
-          <Card className="flex-1 relative">
+
+          <Card className="bg-muted/50 dark:bg-muted flex-1 relative">
             <CardHeader>
-              <CardDescription>Potential Income</CardDescription>
-              <CardTitle className="text-4xl">$0.00</CardTitle>
-              <small className="text-xs text-muted-foreground">
-                For the year {currentYear}
-              </small>
+              <CardDescription>{t("ACTIVE_WORKSPACES")}</CardDescription>
+              <CardTitle className="text-4xl">{activeClients}</CardTitle>
             </CardHeader>
             <CardContent className="text-sm text-muted-foreground">
-              This is how much you can close.
+              {t("ACTIVE_WORKSPACES_DESCRIPTION")}
             </CardContent>
-            <DollarSign className="absolute right-4 top-4 text-muted-foreground" />
+            <UsersRoundIcon className="absolute right-4 top-4 text-muted-foreground" />
           </Card>
-          <Card className="flex-1 relative">
+
+          <Card className="bg-muted/50 dark:bg-muted flex-1 relative">
             <CardHeader>
-              <CardDescription>Active Clients</CardDescription>
-              <CardTitle className="text-4xl">0</CardTitle>
-            </CardHeader>
-            <CardContent className="text-sm text-muted-foreground">
-              Reflects the number of sub-accounts you own and manage.
-            </CardContent>
-            <Contact2 className="absolute right-4 top-4 text-muted-foreground" />
-          </Card>
-          <Card className="flex-1 relative">
-            <CardHeader>
-              <CardTitle>Agency Goal</CardTitle>
+              <CardTitle>{t("AGENCY_GOAL")}</CardTitle>
               <CardDescription>
-                <p className="mt-2">
-                  Reflects the number of sub-accounts you want to own and
-                  manage.
-                </p>
+                <span className="mt-2">{t("AGENCY_GOAL_DESCRIPTION")}</span>
               </CardDescription>
             </CardHeader>
             <CardFooter>
               <div className="flex flex-col w-full">
                 <div className="flex justify-between items-center">
                   <span className="text-muted-foreground text-sm">
-                    Current: 0
+                    {t("CURRENT_GOAL_ACHIEVED", {
+                      GOAL: goalProgress,
+                    })}
                   </span>
-                  <span className="text-muted-foreground text-sm">Goal: 0</span>
+                  <span className="text-muted-foreground text-sm">
+                    {t("CURRENT_GOAL", {
+                      GOAL: currentGoal,
+                    })}
+                  </span>
                 </div>
-                <Progress value={0} />
+                <Progress value={progressPercent} />
               </div>
             </CardFooter>
             <Goal className="absolute right-4 top-4 text-muted-foreground" />
           </Card>
         </div>
-        <div className="flex gap-4 xl:!flex-row flex-col">
-          <Card className="p-4 flex-1">
+        <div className="grid auto-rows-min gap-4 md:grid-cols-4">
+          <Card className="bg-muted/50 dark:bg-muted col-span-3 flex-1">
             <CardHeader>
-              <CardTitle>Transaction History</CardTitle>
+              <CardTitle>{t("TRANSACTION_HISTORY")}</CardTitle>
             </CardHeader>
-            {/* <AreaChart
-              className="text-sm stroke-primary"
-              data={[...([]), ...([])]}
-              index="created"
-              categories={["amount_total"]}
-              colors={["primary"]}
-              yAxisWidth={30}
-              showAnimation={true}
-            /> */}
+            <CardContent>{/* TODO: */}</CardContent>
           </Card>
-          <Card className="xl:w-[400px] w-full">
+          <Card className="bg-muted/50 dark:bg-muted w-full">
             <CardHeader>
-              <CardTitle>Conversions</CardTitle>
+              <CardTitle>{t("CLOSING_RATE")}</CardTitle>
             </CardHeader>
-            <CardContent>
-              {/* <CircleProgress
-                value={10}
-                description={
-                  <>
-                    {(
-                      <div className="flex flex-col">
-                        Abandoned
-                        <div className="flex gap-2">
-                          <ShoppingCart className="text-rose-700" />
-                          {0}
-                        </div>
-                      </div>
-                    )}
-                    {(
-                      <div className="felx flex-col">
-                        Won Carts
-                        <div className="flex gap-2">
-                          <ShoppingCart className="text-emerald-700" />
-                          {0}
-                        </div>
-                      </div>
-                    )}
-                  </>
-                }
-              /> */}
-            </CardContent>
+            <CardContent>{/* TODO: */}</CardContent>
           </Card>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
