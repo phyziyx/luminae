020a7f61db0971e2fa2c172025a8b6a54b862a95
Senenzo <mahad621@gmail.com>
2024-11-06 03:14:15 +0500

    Modified Footer

--- src/app/(home)/components/footer.tsx
+++ src/app/(home)/components/footer.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <footer className="items-center text-center justify-center bg-transparent w-full sticky mb-auto bottom py-8 flex flex-col">
-      <div className="mt-8 text-gray-500 dark:text-gray-100">
-        &copy; 2024 Luminae. All rights reserved.
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+      <div className="containter">
+        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:blur before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
+          <Image
+            src="/assets/bulb.png"
+            height={40}
+            width={40}
+            alt="Logo"
+            className="relative"
+          />
+        </div>
+        <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6">
+          <a href="#">Pricing</a>
+          <a href="#">Documnetation</a>
+          <a href="#">Support</a>
+        </nav>
+        <p className="mt-6">&copy; 2024 Luminae, All Rights Reserved.</p>
       </div>
     </footer>
   );
