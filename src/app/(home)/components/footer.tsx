ef24fb7a3a831fe15dfeb7f3acbe58f7555e4201
phyziyx <96657278+phyziyx@users.noreply.github.com>
2024-11-06 03:37:28 +0500

    Footer adjustments

--- src/app/(home)/components/footer.tsx
+++ src/app/(home)/components/footer.tsx
@@ -1,24 +1,32 @@
 import Image from "next/image";
 
 const Footer = () => {
+  const date = new Date().getFullYear();
+
   return (
-    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+    <footer className="bg-muted dark:bg-muted/60 text-[#BCBCBC] text-sm py-10 text-center">
       <div className="containter">
-        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:blur before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
-          <Image
-            src="/assets/bulb.png"
-            height={40}
-            width={40}
-            alt="Logo"
-            className="relative"
-          />
+        <div className="inline-flex relative">
+          <div className="flex flex-col place-items-center rounded-lg p-2">
+            <Image
+              src="/assets/bulb.png"
+              height={40}
+              width={40}
+              alt="Logo"
+              className="relative"
+            />
+            <Image
+              src="/assets/luminae_plain.png"
+              height={96}
+              width={96}
+              alt="Logo"
+              className="relative"
+            />
+          </div>
         </div>
-        <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6">
-          <a href="#">Pricing</a>
-          <a href="#">Documnetation</a>
-          <a href="#">Support</a>
-        </nav>
-        <p className="mt-6">&copy; 2024 Luminae, All Rights Reserved.</p>
+        <p className="text-black mt-6">
+          &copy; {date} Luminae, All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
