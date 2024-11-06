beb600cfe6f4de5a2dc563fdf72df26a939e00d0
phyziyx <96657278+phyziyx@users.noreply.github.com>
2024-11-07 00:51:43 +0500

    fix: defined but never used

--- src/app/(auth)/accept-invitation/actions.ts
+++ src/app/(auth)/accept-invitation/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import z from "zod";
-import prisma from "@/lib/db";
+// import prisma from "@/lib/db";
 import { formSchema } from "./types";
 
 export async function onFormSubmit(values: z.infer<typeof formSchema>) {
