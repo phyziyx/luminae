50bdeda5474d3be59ca181d730005a4511236fd3
phyziyx <96657278+phyziyx@users.noreply.github.com>
2024-11-09 18:11:58 +0500

    update pricing card

--- src/app/(home)/components/pricing.tsx
+++ src/app/(home)/components/pricing.tsx
@@ -68,54 +68,59 @@ const PricingCard = ({ data }: PricingCardProps) => {
   const t = useTranslations();
 
   return (
-    <Card
-      className={cn(
-        "bg-white dark:bg-muted p-8 border-2 rounded-3xl shadow-md max-w-xs w-full flex flex-col justify-between",
-        {
-          "border-primary": data.popular,
-        }
-      )}
+    <div
+      className={cn({
+        "bg-primary rounded-3xl flex flex-col items-center place-items-center":
+          data.popular,
+      })}
     >
-      <CardHeader className="text-2xl font-bold ">
-        <CardTitle className="flex flex-col items-center -mt-6">
-          {data.popular && (
-            <div className="inline-flex text-sm px-4 py-1.5">
-              <Badge variant={"outline"} className="bg text-base font-bold">
-                <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text">
-                  {t("PRICING_POPULAR")}
-                </span>
-              </Badge>
+      {data.popular && (
+        <Badge variant={"default"} className="p-2 font-bold text-sm text-white">
+          MOST POPULAR
+        </Badge>
+      )}
+      <Card
+        className={cn(
+          "bg-white dark:bg-muted p-8 border-2 rounded-3xl shadow-md max-w-xs w-full flex flex-col justify-between",
+          {
+            "border-primary": data.popular,
+          }
+        )}
+      >
+        <CardHeader className="text-2xl font-bold ">
+          <CardTitle className="flex flex-col items-center -mt-6">
+            {data.title}
+            <div className="flex items-center justify-center gap-1 mt-[30px]">
+              <span className="text-4xl font-bold tracking-tighter leading-none">
+                ${data.monthlyPrice}
+              </span>
+              <span className="text-base tracking-tight font-bold text-black/50 dark:text-gray-400">
+                {t("PRICING_PER_MONTH")}
+              </span>
             </div>
-          )}
-          {data.title}
-          <div className="flex items-center justify-center gap-1 mt-[30px]">
-            <span className="text-4xl font-bold tracking-tighter leading-none">
-              ${data.monthlyPrice}
-            </span>
-            <span className="text-base tracking-tight font-bold text-black/50 dark:text-gray-400">
-              {t("PRICING_PER_MONTH")}
-            </span>
-          </div>
-          <Button
-            className={cn("w-full mt-[30px]")}
-            variant={data.popular ? "default" : "secondary"}
-          >
-            {t("CHOOSE_PLAN")} <ChevronRightIcon />
-          </Button>
-        </CardTitle>
-      </CardHeader>
+            <Button
+              className={cn("w-full mt-[30px]", {
+                "dark:bg-slate-500": !data.popular,
+              })}
+              variant={data.popular ? "default" : "secondary"}
+            >
+              {t("CHOOSE_PLAN")} <ChevronRightIcon />
+            </Button>
+          </CardTitle>
+        </CardHeader>
 
-      <CardFooter>
-        <ul className="flex flex-col gap-5 mt-8">
-          {data.features.map((feature, index) => (
-            <li key={index} className="text-sm flex items-center gap-4">
-              <CheckIcon className="h-6 w-6" />
-              <span>{feature}</span>
-            </li>
-          ))}
-        </ul>
-      </CardFooter>
-    </Card>
+        <CardFooter>
+          <ul className="flex flex-col gap-5 mt-8">
+            {data.features.map((feature, index) => (
+              <li key={index} className="text-sm flex items-center gap-4">
+                <CheckIcon className="h-6 w-6" />
+                <span>{feature}</span>
+              </li>
+            ))}
+          </ul>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
 
