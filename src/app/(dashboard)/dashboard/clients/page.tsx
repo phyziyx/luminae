343ef76090967c3da74ab39e6670d437496d6894
phyziyx <96657278+phyziyx@users.noreply.github.com>
2024-12-31 23:32:16 +0500

    fix: clients header

--- src/app/(dashboard)/dashboard/clients/page.tsx
+++ src/app/(dashboard)/dashboard/clients/page.tsx
@@ -54,7 +54,7 @@ const Clients = async () => {
       <header className="flex h-16 items-center px-4">
         <SidebarTrigger />
         <Separator orientation="vertical" className="mx-2 h-4" />
-        <h1 className="text-3xl font-semibold">{t("CLIENTS.CREATE_CLIENT")}</h1>
+        <h1 className="text-3xl font-semibold">{t("CLIENTS.HEADER")}</h1>
       </header>
       <div className="flex flex-1 flex-col gap-4 p-4">
         <Suspense fallback={<FallbackSpinner />}>
