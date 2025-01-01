337da9b17f435c9a786d314c0d6aa1d5518e2fbd
phyziyx <96657278+phyziyx@users.noreply.github.com>
2025-01-01 20:13:31 +0500

    cleanup team invite toast

--- src/app/(dashboard)/dashboard/team/components/team-invite/team-invite-form.tsx
+++ src/app/(dashboard)/dashboard/team/components/team-invite/team-invite-form.tsx
@@ -52,17 +52,6 @@ export default function TeamInviteForm() {
       toast({
         title: "An error occurred while sending the invitation",
       });
-    } finally {
-      toast({
-        title: "You submitted the following values:",
-        description: (
-          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-            <code className="text-white">
-              {JSON.stringify(values, null, 2)}
-            </code>
-          </pre>
-        ),
-      });
     }
   }
 
