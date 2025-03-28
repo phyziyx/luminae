c3bfa64aa22445d8ad122646b5e2f503a3b227fb
phyziyx <96657278+phyziyx@users.noreply.github.com>
2025-03-28 23:53:28 +0500

    feat: add error handling and empty state message to comments list component
    [skip-ci]

--- src/app/community/components/comments-list.tsx
+++ src/app/community/components/comments-list.tsx
@@ -7,6 +7,7 @@ import Comment from "./comment";
 import { fetchComments } from "@/lib/managers/postManager";
 import { LoadingSpinner } from "@/components/site/loading-spinner";
 import { queryKeys } from "@/lib/react-query";
+import { BirdIcon, WifiOff } from "lucide-react";
 
 function useComments(postId: string) {
   return useSuspenseInfiniteQuery({
@@ -23,7 +24,14 @@ function useComments(postId: string) {
 }
 
 export default function CommentsList({ postId }: { postId: string }) {
-  const { data, hasNextPage, fetchNextPage, isFetching } = useComments(postId);
+  const {
+    data,
+    isError,
+    hasNextPage,
+    fetchNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useComments(postId);
 
   const comments = useMemo(
     () => data?.pages.flatMap((page) => page?.items),
@@ -38,7 +46,19 @@ export default function CommentsList({ postId }: { postId: string }) {
       {comments.map((comment) => (
         <Comment key={comment.id} comment={comment} />
       ))}
-      {isFetching && hasNextPage && (
+      {!isError && !isFetching && (
+        <div className="flex flex-col items-center align-middle justify-center py-4 text-gray-500 dark:text-gray-100 gap-4">
+          No comments yet. Be the first to comment!
+          <BirdIcon size={52} />
+        </div>
+      )}
+      {isError && (
+        <div className="flex flex-col items-center align-middle justify-center py-4 text-gray-500 dark:text-gray-100 gap-4">
+          Error loading comments. Please try again later.
+          <WifiOff size={52} />
+        </div>
+      )}
+      {isFetchingNextPage && (
         <div className="flex justify-center py-4 text-primary">
           <LoadingSpinner />
         </div>
