6dee91f0e20c9d5b6cce346ec141c121b76d6ae9
phyziyx <96657278+phyziyx@users.noreply.github.com>
2025-04-13 23:32:29 +0500

    fix: comment form to only appear for signed-in users.

--- src/app/community/components/comment-section.tsx
+++ src/app/community/components/comment-section.tsx
@@ -3,8 +3,11 @@ import CommentForm from "./comment-form";
 import CommentList from "./comments-list";
 import getQueryClient, { queryKeys } from "@/lib/react-query";
 import { fetchComments } from "@/lib/managers/postManager";
+import { getSession } from "@/lib/auth/auth";
 
 export default async function CommentSection({ postId }: { postId: string }) {
+  const session = await getSession();
+
   const queryClient = getQueryClient();
 
   await queryClient.prefetchInfiniteQuery({
@@ -18,17 +21,17 @@ export default async function CommentSection({ postId }: { postId: string }) {
   });
 
   return (
-    <div>
+    <div className="w-full">
       <h2 className="mb-6 text-2xl font-bold text-gray-800 dark:text-gray-100">
         Comments
         {/* <span className="text-[#5B9AFF] dark:text-[#7BABFF]">
           ({comments.length})
         </span> */}
-        <div className="mt-1 h-1 w-24 bg-[#5B9AFF] dark:bg-[#7BABFF]"></div>
+        <div className="mt-1 h-1 w-24 bg-[#5B9AFF] dark:bg-[#7BABFF]" />
       </h2>
 
       {/* Comment Input */}
-      <CommentForm postId={postId} />
+      {session?.user && <CommentForm postId={postId} />}
 
       {/* Comments List */}
       <HydrationBoundary state={dehydrate(queryClient)}>
