c5151d1c4202a0503f40cccfd9d970c536039545
Ali Khowaja <96657278+phyziyx@users.noreply.github.com>
2025-05-08 12:13:36 +0500

    fix(workspace): manager (#128)

--- src/app/(dashboard)/workspace/[id]/page.tsx
+++ src/app/(dashboard)/workspace/[id]/page.tsx
@@ -53,7 +53,7 @@ export default async function WorkspacePage({
           <Separator orientation="vertical" className="mr-2 h-4" />
           <h1 className="text-3xl font-semibold">{`${t("WORKSPACE")}: ${
             workspace.name
-          }`}</h1>
+          } (isManager: ${manager})`}</h1>
         </div>
       </header>
 
